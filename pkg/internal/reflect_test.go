5f6c50382f2729bd533e691ef84164ca7f51288d
Filip Petrov <lombert.p.v@gmail.com>
2021-04-04 16:31:57 +0300

    Implement test for autowire internal/reflect
    Signed-off-by: Filip Petrov <lombert.p.v@gmail.com>

---
+++ pkg/internal/reflect_test.go
@@ -0,0 +1,31 @@
+package internal
+
+import (
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
+)
+
+const unexportedField = "unexported"
+
+type Foo struct {
+	unexported string
+}
+
+func TestGetUnexportedField(t *testing.T) {
+	foo := &Foo{unexported: "x"}
+	value := GetUnexportedField(reflect.ValueOf(foo).Elem().FieldByName(unexportedField))
+	assert.Equal(t, value, "x")
+}
+
+func TestSetUnexportedField(t *testing.T) {
+	foo := &Foo{unexported: "old"}
+	SetUnexportedField(reflect.ValueOf(foo).Elem().FieldByName(unexportedField), "changed")
+	assert.Equal(t, foo.unexported, "changed")
+}
+
+func TestSetFieldValue(t *testing.T) {
+	foo := &Foo{unexported: "old"}
+	SetFieldValue(reflect.ValueOf(foo).Elem(), 0, "changed")
+	assert.Equal(t, foo.unexported, "changed")
+}
